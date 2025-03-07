beca95ec2a615962dd9c213573cd8aee3dac1007
kreativniynick <kreativniynick@gmail.com>
2025-03-07 19:08:49 +0600

    Add pages design

---
+++ ProjectManager.UI/ViewModels/EmployeeListViewModel.cs
@@ -0,0 +1,9 @@
+﻿using ProjectManager.DataAccess.Models;
+
+namespace ProjectManager.UI.ViewModels
+{
+    public class EmployeeListViewModel
+    {
+        public List<Employee> Employees { get; set; }
+    }
+}
